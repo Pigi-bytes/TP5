43c94fb92a78af498e2311ae008b9b1a405e623f
Mimar999 <152299796+Mimar999@users.noreply.github.com>
2024-03-26 10:28:20 +0100

    Add files via upload

--- main.h
+++ main.h
@@ -1,34 +1,82 @@
 #include <stdio.h>
 
+#define RIVEARRIVEE 1
+#define RIVEDEPART 0
+
 #define CANNIBALE 1
 #define MISSIONNAIRE 0
 
-#define RIVEDEPART 0
-#define RIVEARRIVE 1
-
-typedef struct {
-    int missionnaire;
+typedef struct{
     int cannibale;
+    int missionnaire;
+}rive;
 
-} rive;
+void traverser(rive *riveDepart, rive *riveArrivee, int coteRive, int cOuM, int nombre){
 
-void afficherDebut() {
-    printf("La barque est sur la rive de départ \n");
-    printf("rive de départ : 3 missionnaire(s) et 3 cannibale(s) \n");
-    printf("rive d’arrivée : 0 missionnaire(s) et 0 cannibale(s) \n");
+    if(cOuM == MISSIONNAIRE){
+        if(coteRive == RIVEDEPART){ 
+            riveArrivee->missionnaire += nombre;
+            riveDepart->missionnaire -= nombre;
+        }
+        else if(coteRive = RIVEARRIVEE){ 
+            riveDepart->missionnaire += nombre;
+            riveArrivee->missionnaire -= nombre;
+        }
+    } 
+    else if(cOuM == CANNIBALE){
+        if(coteRive == RIVEDEPART){ 
+            riveArrivee->cannibale += nombre;
+            riveDepart->cannibale -= nombre;
+        }
+        else if(coteRive = RIVEARRIVEE){
+            riveDepart->cannibale += nombre;
+            riveArrivee->cannibale -= nombre;
+        }
+    }
+}
+
+void gagneOuPerduOuRien(rive riveDepart, rive riveArrivee){
+    // Victoire
+    if(riveDepart.missionnaire == 0 && riveDepart.cannibale == 0 && riveArrivee.missionnaire == 3 && riveArrivee.cannibale == 3){
+        gagner();
+    }
+    // Défaite (rive arrivée)
+    else if(riveArrivee.missionnaire < riveArrivee.cannibale){
+        perdu(RIVEARRIVEE);
+    }
+    // Défaite (rive départ)
+    else if(riveDepart.missionnaire < riveDepart.cannibale){
+        perdu(RIVEDEPART);
+    }
+    // Ni perdu ni gagner
+    else {
+        rienFaire();
+    }
 }
 
 int missionnaireTraverser() {
     int valeur;
     printf("Combien de missionnaires voulez-vous faire traverser ?");
     scanf("%d", &valeur);
+    // Condition
+    while(valeur > 2){
+        valeur = 0;
+        printf("Combien de missionnaires voulez-vous faire traverser ?");
+        scanf("%d", &valeur);
+    }
     return valeur;
 }
 
-int missionnaireTraverser() {
+int cannibaleTraverser(int valeurMiss) {
     int valeur;
-    printf("Combien de cannibales voulez-vous faire traverser ?");
+    printf("Combien de cannibale voulez-vous faire traverser ?");
     scanf("%d", &valeur);
+    // Condition
+    while(valeur > 2 && (valeurMiss + valeur) > 2){
+        valeur = 0;
+        printf("Combien de missionnaires voulez-vous faire traverser ?");
+        scanf("%d", &valeur);
+    }
     return valeur;
 }
 
@@ -37,17 +85,28 @@ void afficherRive(rive riveDepart, rive riveArriver) {
     printf("rive de d'arrivée : %d missionnaire(s) et %d cannibale(s)", riveArriver.missionnaire, riveArriver.cannibale);
 }
 
-void afficherTraverser(int missionnaireTraverser, int CannibaleTraverser) {
-    printf("On fait traverser %d missionnaire(s) et %d cannibale(s)", missionnaireTraverser, CannibaleTraverser);
+void afficherTraverser(int missionnaireTraverser, int cannibaleTraverser) {
+    printf("On fait traverser %d missionnaire(s) et %d cannibale(s)", missionnaireTraverser, cannibaleTraverser);
 }
 
-void gagner() {
+int gagner() {
     printf("Vous avez gagné la partie !");
+    return 1;
 }
 
-void perdu(int rivePerdu) {
-    if (rivePerdu == RIVEDEPART) printf("Les cannibales mangent les missionnaires sur la rive de départ !");
-    else printf("Les cannibales mangent les missionnaires sur la rive d'arrivé !");
-        
+int perdu(int rivePerdu) {
+    if (rivePerdu == RIVEDEPART) printf("Les cannibale mangent les missionnaires sur la rive de départ !");
+    else printf("Les cannibale mangent les missionnaires sur la rive d'arrivé !");
+
     printf("Vous avez perdu la partie.");
+    return 0;
+}
+
+void afficherRiveRadeau(int coteRadeau){
+    if(coteRadeau == RIVEDEPART) printf("La barque est sur la rive de départ \n");
+    else printf("La barque est sur la rive d'arrivée");
+}
+
+int rienFaire(){
+    return -1;
 }
